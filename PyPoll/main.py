d446e05d0c5a7f1be64980242ea8106d523d5274
Siddharth Dave <59712314+davesiddharth@users.noreply.github.com>
2020-02-19 00:23:08 -0500

    Update main.py
    Script made independent of the election poll dataset provided.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,16 @@ import os
 import csv
 
 csvpath = os.path.join("C:/Users/siddh/Desktop/Data Analytics Bootcamp/03 PYTHON/Homework/python-challenge/Resources/PyPoll_election_data_subset.csv")
+'''def unique(list):
+    unique_list = []
+    for x in list: 
+        # check if exists in unique_list or not 
+        if x not in unique_list: 
+            unique_list.append(x) 
+    print(unique_list)
+    # print list 
+    #for x in unique_list: 
+       #print(x)'''
 
 #reading csv file
 with open(csvpath, newline='') as csvfile:
@@ -16,46 +26,54 @@ with open(csvpath, newline='') as csvfile:
     #print(f"{csv_header}")
 
     votes = []
+    candidates_list = []
     candidate_names = []
     candidate0_vote = []
     candidate1_vote = []
     candidate2_vote = []
     candidate3_vote = []
-
+    
     for row in csvreader:
         #to calculate total number of votes casted
         voterID = row[0]
         votes.append(voterID)
-
-        #to find each candidate from the dataset and their total vote
-        names = row[2]
-        candidate_names.append(names)
-        candidate_names = list(set(candidate_names))
+        candidate = row[2]
+        candidates_list.append(candidate)
         
-        if row[2] == "Khan": #candidate_names[0]:
-            candidate0_vote.append(row[2])
-        elif row[2] == "Correy": #candidate_names[1]:
-            candidate1_vote.append(row[2])
-        elif row[2] == "Li": #candidate_names[2]:
-            candidate2_vote.append(row[2])
-        elif row[2] == "O'Tooley": #candidate_names[3]:
-            candidate3_vote.append(row[2])
-        
+        for x in candidates_list: 
+        # check if exists in unique_list or not 
+            if x not in candidate_names: 
+                candidate_names.append(x) 
+            
+    #print(candidate_names)
     total_votes = len(votes)
+
+    for candidate in candidates_list:
+        #to count candidates individual votes
+        if candidate == candidate_names[0]:
+            candidate0_vote.append(candidate)
+        elif candidate == candidate_names[1]:
+            candidate1_vote.append(candidate)
+        elif candidate == candidate_names[2]:
+            candidate2_vote.append(candidate)
+        elif candidate == candidate_names[3]:
+            candidate3_vote.append(candidate)
+        
 
     candidate_vote0_percentage = (len(candidate0_vote)/total_votes)*100
     candidate_vote1_percentage = (len(candidate1_vote)/total_votes)*100
     candidate_vote2_percentage = (len(candidate2_vote)/total_votes)*100
     candidate_vote3_percentage = (len(candidate3_vote)/total_votes)*100
 
-    print("Election Results")
-    print("-------------------------------------")
-    print(f"Total Votes: {total_votes}")
-    print(candidate_names)
-    print(f"Khan: {candidate_vote0_percentage}% ({len(candidate0_vote)})")
-    print(f"Correy: {candidate_vote1_percentage}% ({len(candidate1_vote)})")
-    print(f"Li: {candidate_vote2_percentage}% ({len(candidate2_vote)})")
-    print(f"O'Tooley: {candidate_vote3_percentage}% ({len(candidate3_vote)})")
-    print("-------------------------------------")
-    print(f"Winner: ")
-    print("-------------------------------------")
+print("Election Results")
+print("-------------------------------------")
+print(f"Total Votes: {total_votes}")
+print(candidate_names)
+print(f"{candidate_names[0]}: {round(candidate_vote0_percentage,3)}% ({len(candidate0_vote)})")
+print(f"{candidate_names[1]}: {round(candidate_vote1_percentage,3)}% ({len(candidate1_vote)})")
+print(f"{candidate_names[2]}: {round(candidate_vote2_percentage,3)}% ({len(candidate2_vote)})")
+print(f"{candidate_names[3]}: {round(candidate_vote3_percentage,3)}% ({len(candidate3_vote)})")
+print("-------------------------------------")
+print(f"Winner: ")
+print("-------------------------------------")
+
