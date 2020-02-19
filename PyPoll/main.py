978c0ce9f2f82b647be414d02c1c12d8a7055a5d
Siddharth Dave <59712314+davesiddharth@users.noreply.github.com>
2020-02-18 19:58:42 -0500

    Update main.py
    Code working for total votes, total candidate vote count and candidate vote percentage.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,9 +6,6 @@ import csv
 
 csvpath = os.path.join("C:/Users/siddh/Desktop/Data Analytics Bootcamp/03 PYTHON/Homework/python-challenge/Resources/PyPoll_election_data_subset.csv")
 
-#def candidate(candidate_data):
-    #name = candidate_name.append(row[2])
-
 #reading csv file
 with open(csvpath, newline='') as csvfile:
 
@@ -20,44 +17,45 @@ with open(csvpath, newline='') as csvfile:
 
     votes = []
     candidate_names = []
-    candidate_vote = []
-    candidate_vote0 = []
-    candidate_vote1 = []
-    candidate_vote2 = []
-    candidate_vote3 = []
+    candidate0_vote = []
+    candidate1_vote = []
+    candidate2_vote = []
+    candidate3_vote = []
 
     for row in csvreader:
         #to calculate total number of votes casted
         voterID = row[0]
         votes.append(voterID)
 
-        #to find each candidate from the dataset
-        #candidate(row)
+        #to find each candidate from the dataset and their total vote
         names = row[2]
         candidate_names.append(names)
         candidate_names = list(set(candidate_names))
-
-        if candidate_names[0] == row[2]:
-            candidate_vote0.append(row[0])
-        elif candidate_names[1] == row[2]:
-            candidate_vote1.append(row[0])
-        elif candidate_names[2] == row[2]:
-            candidate_vote2.append(row[0])
-        else:
-            candidate_vote3.append(row[0])
         
-    
+        if row[2] == "Khan": #candidate_names[0]:
+            candidate0_vote.append(row[2])
+        elif row[2] == "Correy": #candidate_names[1]:
+            candidate1_vote.append(row[2])
+        elif row[2] == "Li": #candidate_names[2]:
+            candidate2_vote.append(row[2])
+        elif row[2] == "O'Tooley": #candidate_names[3]:
+            candidate3_vote.append(row[2])
+        
     total_votes = len(votes)
-    candidate_vote0_total = len(candidate_vote0)
-    candidate_vote1_total = len(candidate_vote1)
-    candidate_vote2_total = len(candidate_vote2)
-    candidate_vote3_total = len(candidate_vote3)
+
+    candidate_vote0_percentage = (len(candidate0_vote)/total_votes)*100
+    candidate_vote1_percentage = (len(candidate1_vote)/total_votes)*100
+    candidate_vote2_percentage = (len(candidate2_vote)/total_votes)*100
+    candidate_vote3_percentage = (len(candidate3_vote)/total_votes)*100
 
     print("Election Results")
     print("-------------------------------------")
-    print(f"Election Results: {total_votes}")
-    print(f"{candidate_names[0]}: ({candidate_vote0_total})")
-    print(f"{candidate_names[1]}: ({candidate_vote1_total})")
-    print(f"{candidate_names[2]}: ({candidate_vote2_total})")
-    print(f"{candidate_names[3]}: ({candidate_vote3_total})")
-
+    print(f"Total Votes: {total_votes}")
+    print(candidate_names)
+    print(f"Khan: {candidate_vote0_percentage}% ({len(candidate0_vote)})")
+    print(f"Correy: {candidate_vote1_percentage}% ({len(candidate1_vote)})")
+    print(f"Li: {candidate_vote2_percentage}% ({len(candidate2_vote)})")
+    print(f"O'Tooley: {candidate_vote3_percentage}% ({len(candidate3_vote)})")
+    print("-------------------------------------")
+    print(f"Winner: ")
+    print("-------------------------------------")
